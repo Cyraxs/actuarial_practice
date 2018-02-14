4008a72c8040aed127d9e0af1fb15aa15d2a2d98
Cyraxs <Cyraxs@users.noreply.github.com>
2018-02-13 21:13:25 -0600

    Add files via upload

---
+++ workers_compesation_ibnr.py
@@ -0,0 +1,23 @@
+#See: http://chainladder-python.readthedocs.io/en/master/BootstrapExample.html#generate-bootstrap-chainladder-model
+# The CAS Loss Reserving incurred data is schedule P data
+# and would include include IBNR. Additionally,
+# the database includes the completed triangles
+# (i.e. the latest accident year has valuations at age 10 years).
+# We will focus on Workers’ Compensation data.
+
+import chainladder as cl
+import pandas as pd
+import numpy as np
+%matplotlib inline
+
+
+
+
+
+def main():
+    # my code here
+
+
+if __name__ == "__main__":
+    #call main function
+    main()
